22ecda95f4e25df35b947f579b5f3511fb390a1f
shhubhhh <kaushikshubham09@gmail.com>
2024-03-08 17:35:09 +0530

    create signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,29 @@
+import User from "../models/User.model.js";
+import bcryptjs from  'bcryptjs'
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === " " ||
+    email === " " ||
+    password === " "
+  ) {
+    return res.status(400).json({ msg: "All fields are required" });
+  }
+
+  const hashedPassword = bcryptjs.hashSync(password, 10)
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
+  try {
+    await newUser.save();
+    res.json("Signup succesful");
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
--- api/index.js
+++ api/index.js
@@ -2,8 +2,9 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoute.js";
-const app = express();
+import authRoutes from "./routes/authRoute.js";
 
+const app = express();
 
 dotenv.config();
 
@@ -16,8 +17,11 @@ mongoose
     console.log(err);
   });
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
 app.listen(3000, () => {
   console.log("Server is running  on port 3000");
 });
-
-app.use('/api/user', userRoutes);
